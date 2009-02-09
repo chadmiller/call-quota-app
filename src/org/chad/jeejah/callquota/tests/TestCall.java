b49028a8d99e3ecee80666dc8920cf3b703fb9a1
Chad Miller <chad@>
2009-02-08 23:27:18 -0500

    Strip out unused tests.

--- src/org/chad/jeejah/callquota/tests/TestCall.java
+++ src/org/chad/jeejah/callquota/tests/TestCall.java
@@ -8,15 +8,5 @@ import junit.framework.*;
 
 public class TestCall {
 
-    @Test
-    public void test_getNormalizedNumber() {
-        Assert.assertEquals("1", "1", Call.getNormalizedNumber("1"));
-        Assert.assertEquals("2", "1", Call.getNormalizedNumber("+1"));
-        Assert.assertEquals("3", "123", Call.getNormalizedNumber("+1-23"));
-        Assert.assertEquals("4", "1234567", Call.getNormalizedNumber("+1-23 4 5 67"));
-        Assert.assertEquals("5", "1234567", Call.getNormalizedNumber(" \000 + 1 -23 4 5 6   \n\rabc   7 "));
-        Assert.assertEquals("6", "", Call.getNormalizedNumber(""));
-    }
-
 }
 /* vim: set et ai sta : */
